e4738198f76d3d569f99fe7f9521a4831fc5b0d9
Maerodrim <stas.serg.99@mail.ru>
2019-09-15 12:45:23 +0400

    Make 2.1-2.6 but no test

--- src/main/java/ru/ssau/tk/sergunin/practoop/MyArray/MyArray.java
+++ src/main/java/ru/ssau/tk/sergunin/practoop/MyArray/MyArray.java
@@ -3,24 +3,67 @@ package ru.ssau.tk.sergunin.practoop.MyArray;
 import java.util.Arrays;
 
 public class MyArray {
-    private double[] xValues;
-    private int count;
-
-    MyArray(double[] xValues) {
-        this.count = xValues.length;
-        this.xValues = Arrays.copyOf(xValues, count);
+    private MyArray() {
+        ;
     }
 
-    MyArray(int count) {
+    public double[] GeneratorNewArray(int count) {
+        double[] yValues = new double[count];
+        for (int i = 0; i < (count); i++) {
+            yValues[i] = 0;
+        }
+        return yValues;
+    }
+
+    public double[] GeneratorTwoOneTwo(int count) {
+        double[] yValues = new double[count];
+        if (count > 0) {
+            yValues[0] = 2;
+            yValues[count - 1] = 2;
+            for (int i = 1; i < (count - 1); i++) {
+                yValues[i] = 1;
+            }
+        }
+        return yValues;
+    }
+
+    public double[] GeneratorSqr(int count) {
         double[] yValues = new double[count];
         for (int i = 0; i < count; i++) {
-            yValues[i] = i;
+            yValues[i] = i * i;
         }
-        this.count = yValues.length;
-        this.xValues = Arrays.copyOf(yValues, count);
+        return yValues;
     }
 
-    public  int getCount() {
-        return count;
+    public double[] GeneratorOdd(int count) {
+        double[] yValues = new double[count];
+        for (int i = 0; i < count; i++) {
+            yValues[i] = (2 * i + 1);
+        }
+        return yValues;
+    }
+
+    public double[] GeneratorDecreasingEven(int count) {
+        double[] yValues = new double[count];
+        for (int i = 0; i < count; i++) {
+            yValues[i] = (2 * (count - i));
+        }
+        return yValues;
+    }
+
+    public double[] GeneratorFibonacciNumbers(int count) {
+        double[] yValues = new double[count];
+        if (count > 0) {
+            yValues[0] = 1;
+            if (count > 1) {
+                yValues[1] = 1;
+                if (count > 2) {
+                    for (int i = 2; i < count; i++) {
+                        yValues[i] = (yValues[i - 2] + yValues[i - 1]);
+                    }
+                }
+            }
+        }
+        return yValues;
     }
 }
